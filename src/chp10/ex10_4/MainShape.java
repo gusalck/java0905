37db4ce51b05a009f21aeb57bed00f4a13526267
gusalck <gkrrywlrwkd@gmail.com>
2024-10-17 09:45:38 +0900

    업캐스팅 : 실습 10-4(ArrayList)

--- src/chp10/ex10_4/MainShape.java
+++ src/chp10/ex10_4/MainShape.java
@@ -1,7 +1,11 @@
 package chp10.ex10_4;
 
+import java.util.ArrayList;
+
 public class MainShape {
     public static void main(String[] args) {
+        ArrayList<Shape> shapes = new ArrayList<>();
+
         Square s = new Square();
         s.name = "정사각형";
         Triangle t = new Triangle();
@@ -9,9 +13,13 @@ public class MainShape {
         Circle c = new Circle();
         c.name = "원";
 
-        Shape[] shapes = {s, t, c};
-        for (int i = 0; i < shapes.length; i++) {
-            System.out.printf("%d번의 인덱스의 도형 : %s\n", i, shapes[i].name);
+        shapes.add(s);
+        shapes.add(t);
+        shapes.add(c);
+
+
+        for (int i = 0; i < shapes.size(); i++) {
+            System.out.printf("%d번의 인덱스의 도형 : %s\n", i, shapes.get(i).name);
         }
     }
 }
