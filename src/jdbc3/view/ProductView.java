80aff176aa5999773eee0a93f411b979c93e4684
gusalck <gkrrywlrwkd@gmail.com>
2024-10-30 11:43:31 +0900

    JDBC Programming & 패키지를 MVC 모델링에 따라 분류하여 사용2

---
+++ src/jdbc3/view/ProductView.java
@@ -0,0 +1,35 @@
+package jdbc3.view;
+
+import jdbc3.entity.Product;
+
+import java.util.ArrayList;
+
+public class ProductView {
+    public void printHeader() {
+        System.out.println("*********************************");
+        System.out.println(" --------- PRODUCT LIST --------- ");
+        System.out.println("*********************************");
+    }
+    public void printFooter() {
+        System.out.println("******************************");
+        System.out.println("--------- END PRINT ----------");
+        System.out.println("******************************");
+    }
+
+    public void printProduct(ArrayList<Product> proList) {
+        for (Product product : proList) {
+            System.out.println("- 제품번호 : "+product.getProNum());
+            System.out.println("- 제품명 : "+product.getProName());
+            System.out.println("- 재고량 : "+product.getProStock());
+            System.out.println("- 단가 : "+product.getProPrice());
+            System.out.println("- 제조업체 : "+product.getProduceName());
+            System.out.println();
+        }
+    }
+    public static void printView(ArrayList<Product> arrList){
+        ProductView view = new ProductView();
+        view.printHeader();
+        view.printProduct(arrList);
+        view.printFooter();
+    }
+}
